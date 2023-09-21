99e24849d8ad556677bcc21216473c803452b067
Abel Rodríguez <Abeltrg@users.noreply.github.com>
2023-09-21 16:33:59 +0000

    primer commit

---
+++ my-first-project/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { EVENT_MANAGER_PLUGINS } from '@angular/platform-browser';
+
+function* generator(){
+  for(var i=0;i<31;i++){
+    yield i+1;
+  }
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'my-first-project';
+  calendarName = 'Calendario de Abel';
+  array = [...generator()];
+  user = "Abel"
+
+  isLogin():boolean {
+    return this.user != undefined;
+  }
+  handlerClick(ev:Event) {
+
+    ev.stopPropagation
+
+    const node = ev.composedPath().map(node => node as HTMLElement).find(n=>n.dataset && 'day' in n.dataset)
+    if (node) {
+      const {day} = node.dataset;      
+      alert('Hoy es el día ' + day + ' del mes');  
+    }
+
+    /*const element = ev.target as HTMLElement
+    const {day} = element.dataset
+    alert('Hoy es el día ' + day + ' del mes');*/
+  }
+}
